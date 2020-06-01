f0a7a538739a1774eada989474c2b018a77c769b
traysimay <teh@raptor.pizza>
2020-06-01 00:49:47 -0700

    clock

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,7 @@ import headerStyles from "./header.module.scss"
 const Header = () => {
   const [display, setDisplay] = useState(false)
   const dropDown = useRef()
+  const canvasRef = useRef()
 
   const openDropDown = () => setDisplay(true)
   const closeDropDown = () => setDisplay(false)
@@ -21,6 +22,26 @@ const Header = () => {
     }
   }, [display])
 
+  useEffect(() => {
+    const ctx = canvasRef.current.getContext("2d")
+    console.log(ctx)
+    const time = new Date()
+    const mins = time.getMinutes()
+    const hrs = time.getHours()
+
+    const numLines = mins + hrs * 60
+    const r = 200
+    const step = Math.PI / (60 * 24)
+    for (let i = 0; i <= numLines; i++) {
+      const x = r * Math.cos(step * i)
+      const y = r * Math.sin(step * i)
+      ctx.beginPath()
+      ctx.moveTo(200, 0)
+      ctx.lineTo(x + 200, y - 20)
+      ctx.stroke()
+    }
+  }, [])
+
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -34,36 +55,44 @@ const Header = () => {
   console.log(display)
   return (
     <header className={headerStyles.header}>
+      <canvas
+        ref={canvasRef}
+        width="400"
+        height="400"
+        className={headerStyles.canvas}
+      />
       <Link className="link" to="/">
         <div className={headerStyles.title}>{data.site.siteMetadata.title}</div>
       </Link>
       <div
-        onClick={!display && openDropDown}
+        onClick={!display ? openDropDown : () => {}}
         ref={dropDown}
         className={`${headerStyles.navList} ${
           display ? headerStyles.navListShow : headerStyles.navListHide
         }`}
       >
-        <ul>
-          <div onClick={closeDropDown} className={headerStyles.x}>
-            X
-          </div>
-          <li>
-            <Link className={headerStyles.link} to="/blog">
-              blog
-            </Link>
-          </li>
-          <li>
-            <Link className={headerStyles.link} to="/blog">
-              playlists
-            </Link>
-          </li>
-          <li>
-            <Link className={headerStyles.link} to="/blog">
-              culture
-            </Link>
-          </li>
-        </ul>
+        {display && (
+          <ul>
+            <div onClick={closeDropDown} className={headerStyles.x}>
+              X
+            </div>
+            <li>
+              <Link className={headerStyles.link} to="/blog">
+                blog
+              </Link>
+            </li>
+            <li>
+              <Link className={headerStyles.link} to="/blog">
+                playlists
+              </Link>
+            </li>
+            <li>
+              <Link className={headerStyles.link} to="/blog">
+                culture
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </header>
   )
