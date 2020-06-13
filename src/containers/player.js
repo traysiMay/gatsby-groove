ae20680ea1c76b1aca219739e56fc5a8da12ef20
traysimay <teh@raptor.pizza>
2020-06-13 16:51:36 -0700

    ssr window check

--- src/containers/player.js
+++ src/containers/player.js
@@ -26,80 +26,83 @@ const Player = () => {
       if (event.key !== "token") return
       setToken(event.newValue)
     }
-    if (window !== "undefined") {
-      window.addEventListener("storage", handlerEvent, false)
+    try {
+      if (window !== "undefined") {
+        window.addEventListener("storage", handlerEvent, false)
 
-      let player = new window.Spotify.Player({
-        name: "Groove Devotion Player",
-        getOAuthToken: cb => {
-          cb(token)
-        },
-      })
-      // Error handling
-      player.addListener("initialization_error", ({ message }) => {
-        console.error(message)
-      })
-      player.addListener("authentication_error", async ({ message }) => {
-        const newToken = await fetch(
-          `${
-            process.env.GATSBY_SERVER_URL
-          }/new-token?refreshToken=${localStorage.getItem("rToken")}`
-        ).then(r => r.json())
-        localStorage.setItem("token", newToken.token)
-        player = new window.Spotify.Player({
+        let player = new window.Spotify.Player({
           name: "Groove Devotion Player",
           getOAuthToken: cb => {
             cb(token)
           },
         })
-        player.connect()
-        console.error(message)
-      })
-      player.addListener("account_error", ({ message }) => {
-        console.error(message)
-      })
-      player.addListener("playback_error", ({ message }) => {
-        console.error(message)
-      })
+        // Error handling
+        player.addListener("initialization_error", ({ message }) => {
+          console.error(message)
+        })
+        player.addListener("authentication_error", async ({ message }) => {
+          const newToken = await fetch(
+            `${
+              process.env.GATSBY_SERVER_URL
+            }/new-token?refreshToken=${localStorage.getItem("rToken")}`
+          ).then(r => r.json())
+          localStorage.setItem("token", newToken.token)
+          player = new window.Spotify.Player({
+            name: "Groove Devotion Player",
+            getOAuthToken: cb => {
+              cb(token)
+            },
+          })
+          player.connect()
+          console.error(message)
+        })
+        player.addListener("account_error", ({ message }) => {
+          console.error(message)
+        })
+        player.addListener("playback_error", ({ message }) => {
+          console.error(message)
+        })
 
-      // Playback status updates
-      player.addListener("player_state_changed", state => {
-        console.log("state change")
-        console.log(state)
-        console.log(state.track_window)
-        const currentTrack = state.track_window.current_track
-        const paused = state.paused
-        try {
-          const cPlaying = document.querySelector(".playing")
-          if (cPlaying) {
-            cPlaying.classList.remove("playing")
+        // Playback status updates
+        player.addListener("player_state_changed", state => {
+          console.log("state change")
+          console.log(state)
+          console.log(state.track_window)
+          const currentTrack = state.track_window.current_track
+          const paused = state.paused
+          try {
+            const cPlaying = document.querySelector(".playing")
+            if (cPlaying) {
+              cPlaying.classList.remove("playing")
+            }
+            const element = document.getElementById(
+              currentTrack.uri.split(":")[2]
+            )
+            element.classList.add("playing")
+          } catch (err) {
+            console.log("cant find this boy")
           }
-          const element = document.getElementById(
-            currentTrack.uri.split(":")[2]
-          )
-          element.classList.add("playing")
-        } catch (err) {
-          console.log("cant find this boy")
-        }
-        setTrack(currentTrack)
-        setIsPlaying(!paused)
-      })
+          setTrack(currentTrack)
+          setIsPlaying(!paused)
+        })
 
-      // Ready
-      player.addListener("ready", ({ device_id }) => {
-        console.log("Ready with Device ID", device_id)
-        localStorage.setItem("deviceId", device_id)
-      })
+        // Ready
+        player.addListener("ready", ({ device_id }) => {
+          console.log("Ready with Device ID", device_id)
+          localStorage.setItem("deviceId", device_id)
+        })
 
-      // Not Ready
-      player.addListener("not_ready", ({ device_id }) => {
-        console.log("Device ID has gone offline", device_id)
-      })
+        // Not Ready
+        player.addListener("not_ready", ({ device_id }) => {
+          console.log("Device ID has gone offline", device_id)
+        })
 
-      // Connect to the player!
-      player.connect()
+        // Connect to the player!
+        player.connect()
+      }
+    } catch (err) {
+      console.log(err, " problem loading spot plaer")
     }
-
     return () => window.removeEventListener("storage", handlerEvent, false)
   }, [token])
 
