50b80e8b448c1334b29393c70840cf3fdff2faa9
traysimay <teh@raptor.pizza>
2020-06-13 16:49:10 -0700

    ssr window check

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,17 @@
 import React from "react"
 import Player from "./src/containers/player"
 
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="https://sdk.scdn.co/spotify-player.js"
+      src="https://sdk.scdn.co/spotify-player.js"
+      crossOrigin="anonymous"
+      defer
+    />,
+  ])
+}
+
 export const wrapPageElement = ({ element, props }) => {
   return (
     <div>
